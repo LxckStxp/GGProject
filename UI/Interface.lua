d80a6d55d2fd96b408c946ee64ce6b0190f8ae22
LxckStxp <lxckstxp@gmail.com>
2025-03-08 15:08:21 +1000

    Update Interface.lua

--- UI/Interface.lua
+++ UI/Interface.lua
@@ -1,38 +1,36 @@
 -- GGProject/UI/Interface.lua
 -- UI setup and controls
 
-local ScriptLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/GGProject/main/Lib/ScriptLoader.lua"))()
-local Config = ScriptLoader:Load("/Core/Config.lua")
-local Censura = ScriptLoader:Load("/UI/CensuraLoader.lua")
+return function(Config, Censura)
+    local Interface = {}
 
-local Interface = {}
+    function Interface:SetupUI()
+        local GUI = Censura.new("GGProject")
+        
+        GUI:CreateToggle("ESP Enabled", false, function(state)
+            Config.ESP.Enabled = state
+        end)
+        
+        GUI:CreateToggle("Aimbot Enabled", false, function(state)
+            Config.Aimbot.Enabled = state
+        end)
+        
+        GUI:CreateSlider("Aimbot FOV", 50, 500, 200, function(value)
+            Config.Aimbot.FOV = value
+        end)
+        
+        GUI:CreateButton("Target: Players", function()
+            Config.Aimbot.TargetMode = "Players"
+        end)
+        
+        GUI:CreateButton("Target: NPCs", function()
+            Config.Aimbot.TargetMode = "NPCs"
+        end)
+        
+        GUI:CreateButton("Target: Both", function()
+            Config.Aimbot.TargetMode = "Both"
+        end)
+    end
 
-function Interface:SetupUI()
-    local GUI = Censura.new("GGProject")
-    
-    GUI:CreateToggle("ESP Enabled", false, function(state)
-        Config.ESP.Enabled = state
-    end)
-    
-    GUI:CreateToggle("Aimbot Enabled", false, function(state)
-        Config.Aimbot.Enabled = state
-    end)
-    
-    GUI:CreateSlider("Aimbot FOV", 50, 500, 200, function(value)
-        Config.Aimbot.FOV = value
-    end)
-    
-    GUI:CreateButton("Target: Players", function()
-        Config.Aimbot.TargetMode = "Players"
-    end)
-    
-    GUI:CreateButton("Target: NPCs", function()
-        Config.Aimbot.TargetMode = "NPCs"
-    end)
-    
-    GUI:CreateButton("Target: Both", function()
-        Config.Aimbot.TargetMode = "Both"
-    end)
+    return Interface
 end
-
-return Interface
