66c5da1ae7afe3e3679a22b46fee159b3c14e175
LxckStxp <lxckstxp@gmail.com>
2025-03-08 14:37:31 +1000

    Create CensuraLoader.lua

---
+++ UI/CensuraLoader.lua
@@ -0,0 +1,5 @@
+-- GGProject/UI/CensuraLoader.lua
+-- Loads Censura UI
+
+local Censura = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/Censura/main/CensuraDev.lua"))()
+return Censura
