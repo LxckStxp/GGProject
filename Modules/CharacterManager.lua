e61c97920f136bb45f949fec6a01fb287284d31c
LxckStxp <lxckstxp@gmail.com>
2025-03-08 15:07:45 +1000

    Update CharacterManager.lua

--- Modules/CharacterManager.lua
+++ Modules/CharacterManager.lua
@@ -1,24 +1,22 @@
 -- GGProject/Modules/CharacterManager.lua
 -- Character event handling
 
-local ScriptLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/GGProject/main/Lib/ScriptLoader.lua"))()
-local Services = ScriptLoader:Load("/Core/Services.lua")
-local EntityTracker = ScriptLoader:Load("/Modules/EntityTracker.lua")
+return function(Services, EntityTracker)
+    local CharacterManager = {}
 
-local CharacterManager = {}
+    function CharacterManager:Init()
+        Services.Workspace.DescendantAdded:Connect(function(descendant)
+            if descendant:IsA("Model") and descendant:FindFirstChild("Humanoid") then
+                EntityTracker:Update()
+            end
+        end)
+        
+        Services.Workspace.DescendantRemoving:Connect(function(descendant)
+            if descendant:IsA("Model") and descendant:FindFirstChild("Humanoid") then
+                EntityTracker:Update()
+            end
+        end)
+    end
 
-function CharacterManager:Init()
-    Services.Workspace.DescendantAdded:Connect(function(descendant)
-        if descendant:IsA("Model") and descendant:FindFirstChild("Humanoid") then
-            EntityTracker:Update()
-        end
-    end)
-    
-    Services.Workspace.DescendantRemoving:Connect(function(descendant)
-        if descendant:IsA("Model") and descendant:FindFirstChild("Humanoid") then
-            EntityTracker:Update()
-        end
-    end)
+    return CharacterManager
 end
-
-return CharacterManager
